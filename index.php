18812b519ca94a7437861d8b3184c29426590e33
asihnurannisa <asihnurannisa@gmail.com>
2025-11-07 21:09:51 +0700

    Rebuild index.php final di main

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
     <title>Halaman Utama</title>
 </head>
 <body>
     <h1>Selamat Datang</h1>
-    <a href="koneksi.php">Tes Koneksi Database</a><br>
-    <a href="tambah.php">Tambah Data Mahasiswa</a>
+
+    <a href="koneksi.php">Tes Koneksi Database</a> <br><br>
+    <a href="tambah.php">Tambah Data</a>
+
 </body>
 </html>
