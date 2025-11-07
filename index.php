55030864ad9e65afb008aeae3324128db74fc6b5
asihnurannisa <asihnurannisa@gmail.com>
2025-11-07 20:52:34 +0700

    Add index.php di branch main

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-echo "<h1>Halaman Utama</h1>" > index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Utama</title>
+</head>
+<body>
+    <h1>Selamat Datang</h1>
+    <a href="koneksi.php">Tes Koneksi Database</a><br>
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+</body>
+</html>
